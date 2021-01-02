3972e540e35f34b4b7edaa83ec6886968dca9181
Osamu Hashimoto <hashimoto.osamu@gmail.com>
2021-01-03 02:45:57 +0900

    DDL, DMLを追加。

---
+++ DML/insert_into_test_db.sql
@@ -0,0 +1,3 @@
+insert into testdb.test_table(id,name) values (1,'Les Dungate');
+insert into testdb.test_table(id,name) values (2,'橋本　修一');
+insert into testdb.test_table(id,name) values (3,'橋本　亜妃子');
